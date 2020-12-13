008b9c74da3214411e82922f53032e6cea75a7df
shawn <ShawnTotaram@hotmail.com>
2020-12-13 12:38:42 -0500

    Ticket Quote

--- app/src/main/java/com/example/lawyerup2/aboutticket.java
+++ app/src/main/java/com/example/lawyerup2/aboutticket.java
@@ -3,23 +3,46 @@ package com.example.lawyerup2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 public class aboutticket extends AppCompatActivity {
-
+    DatePicker ticketDate;
+    DatePicker courtDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutticket);
+        SharedPreferences sharedPreferences
+                = getSharedPreferences("MySharedPref",
+                MODE_PRIVATE);
+        SharedPreferences.Editor myEdit
+                = sharedPreferences.edit();
         Button btn4 = findViewById(R.id.aboutcontinue);
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ticketassigned.class);
-                startActivity(intent);
+                ticketDate=(DatePicker)findViewById(R.id.ticketDate);
+                courtDate=(DatePicker)findViewById(R.id.courtDate);
+                String test = "Selected Date: " + ticketDate.getDayOfMonth() + "/" + (ticketDate.getMonth() + 1) + "/" + ticketDate.getYear();
+                String test2 = "Selected Date: " + courtDate.getDayOfMonth() + "/" + (courtDate.getMonth() + 1) + "/" + courtDate.getYear();
+                myEdit.putString(
+                        "ticketdate",
+                        test);
+                myEdit.putString(
+                        "courtdate",
+                        test2);
+                myEdit.commit();
+                next();
             }
         });
+    }
+
+    public void next(){
+        Intent intent = new Intent(getApplicationContext(), ticketassigned.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/lawyerup2/ticketquote.java
+++ app/src/main/java/com/example/lawyerup2/ticketquote.java
@@ -2,8 +2,12 @@ package com.example.lawyerup2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 public class ticketquote extends AppCompatActivity {
@@ -21,7 +25,25 @@ public class ticketquote extends AppCompatActivity {
         String s2 = sh.getString("assigned", "");
         TextView err1 = (TextView)findViewById(R.id.textView15);
         err1.setText(s2);
+        String ticketDate = sh.getString("ticketdate", "");
+        TextView ticketdate = (TextView)findViewById(R.id.ticketDate);
+        ticketdate.setText(ticketDate);
+        String courtDate = sh.getString("courtdate", "");
+        TextView courtdate = (TextView)findViewById(R.id.courtDate);
+        courtdate.setText(courtDate);
+        Button btn4 = findViewById(R.id.exitCase);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                next();
+            }
+        });
 
     }
 
+    public void next(){
+        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(intent);
+    }
 }
