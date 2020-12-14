ce915774f5536b68a7a313188eb3d0be105616b0
sameermkhan@hotmail.com <Shavane1!>
2020-12-14 03:39:00 -0400

    added RSS feed to Home fragment, changed name in toolbar to be more appropriate.

---
+++ app/src/main/java/com/example/lawyerup2/Interface/ItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.lawyerup2.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+
+    void onClick(View view, int position, boolean isLongClick);
+
+
+
+}
